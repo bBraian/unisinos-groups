2f24a7bf98a51c552ce6eec7734c5a601be7a330
Braian <braianvoficial@gmail.com>
2022-08-11 17:14:17 -0300

    adding some data

--- src/data/groups.js
+++ src/data/groups.js
@@ -1,12 +1,12 @@
 //1 Análise e desenvolvimento de sistemas
 
 export const grupos = [
-    { course: 1, text: 'Desenvolvimento web', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTR_dzmocOz3i9s4s188Mt4AZ5rlTsodgpWdKRWg0SvOd-SpSsgl6ivJkPlsEiklZKxSk&usqp=CAU', whatsapp_link: 'https://chat.whatsapp.com/LNnQkygixWMCGU97cAZdVL' },
-    { course: 1, text: 'Programação I', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Remi_turtlegrafik.png', whatsapp_link: true },
-    { course: 1, text: 'Fundamentos de banco de dados', image: 'green', whatsapp_link: true },
-    { course: 1, text: 'Gerenciamento de Software', image: 'yellow', whatsapp_link: true },
-    { course: 1, text: 'Laboratório II', image: 'gray', whatsapp_link: true },
-    { course: 1, text: 'Cultura, Ética e Diferença', image: 'orange', whatsapp_link: true },
+    { course: 1, text: 'Desenvolvimento Web', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTR_dzmocOz3i9s4s188Mt4AZ5rlTsodgpWdKRWg0SvOd-SpSsgl6ivJkPlsEiklZKxSk&usqp=CAU', whatsapp_link: 'https://chat.whatsapp.com/E1QRZ48cQ1U62ThrllpmF4'},
+    { course: 1, text: 'Programação FrontEnd', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Remi_turtlegrafik.png', whatsapp_link: 'https://chat.whatsapp.com/LNsCS8pf5TF3d56CR6nOBl'},
+    { course: 1, text: 'Arquitetura de Software', image: 'green', whatsapp_link: 'https://chat.whatsapp.com/HvVqtjvVcYy14EvwbZs9ns'},
+    { course: 1, text: 'Segurança da Informação', image: 'yellow', whatsapp_link: 'https://chat.whatsapp.com/LTaeveve7JsAqNHJrkGrGq'},
+    { course: 1, text: 'Projeto Final I', image: 'gray', whatsapp_link: 'https://chat.whatsapp.com/Lhk3G6AxRMD0Wayi4vWDeU'},
+    { course: 1, text: 'Projeto Final II', image: 'orange', whatsapp_link: 'https://chat.whatsapp.com/HhQJjftPEdV71nWXeayRfh'},
     { course: 1, text: 'Arquitetura de Software', image: 'brown', whatsapp_link: true },
     { course: 1, text: 'Arquitetura de Software', image: 'brown', whatsapp_link: true },
     { course: 1, text: 'Arquitetura de Software', image: 'brown', whatsapp_link: true },
