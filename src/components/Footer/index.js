33f0e815dca76c308c1689edbdfca0fff736333a
bBraian <braianvoficial@gmail.com>
2022-08-11 22:08:22 -0300

    updating suport modal text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
     function openSuportModal() {
         Swal.fire({
             title: 'Atenção',
-            text: 'Somente use o suporte para mandar novas cadeiras a serem adicionadas ou para relatar problemas, se não, não enche meu saco por favor, com amor, Braian',
+            text: 'Use o suporte somente para mandar novas cadeiras a serem adicionadas ou para relatar problemas ou correções!',
             icon: 'warning',
             showCancelButton: true,
             confirmButtonText: 'Continuar',
