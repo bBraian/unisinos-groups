ba89988afb5ca2de9b9a2ba845e19d5e01813ad5
Braian <braianvoficial@gmail.com>
2022-08-12 13:46:30 -0300

    saving

--- src/data/groups.js
+++ src/data/groups.js
@@ -2,7 +2,7 @@
 
 export const grupos = [
     { course: 1, text: 'Desenvolvimento Web', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTR_dzmocOz3i9s4s188Mt4AZ5rlTsodgpWdKRWg0SvOd-SpSsgl6ivJkPlsEiklZKxSk&usqp=CAU', whatsapp_link: 'https://chat.whatsapp.com/E1QRZ48cQ1U62ThrllpmF4'},
-    { course: 1, text: 'Programação FrontEnd', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Remi_turtlegrafik.png', whatsapp_link: 'https://chat.whatsapp.com/LNsCS8pf5TF3d56CR6nOBl'},
+    { course: 1, text: 'Programação FrontEnd', image: 'https://i.imgur.com/F28Pggd.png', whatsapp_link: 'https://chat.whatsapp.com/LNsCS8pf5TF3d56CR6nOBl'},
     { course: 1, text: 'Arquitetura de Software', image: 'green', whatsapp_link: 'https://chat.whatsapp.com/HvVqtjvVcYy14EvwbZs9ns'},
     { course: 1, text: 'Segurança da Informação', image: 'yellow', whatsapp_link: 'https://chat.whatsapp.com/LTaeveve7JsAqNHJrkGrGq'},
     { course: 1, text: 'Projeto Final I', image: 'gray', whatsapp_link: 'https://chat.whatsapp.com/Lhk3G6AxRMD0Wayi4vWDeU'},
