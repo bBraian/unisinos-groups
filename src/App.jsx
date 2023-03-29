1e51b2a7ead59757c546bc5b0f328bf5bd1c370f
Braian <braianvoficial@gmail.com>
2023-03-29 09:13:20 -0300

    adding analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [selectedClass, setSelectedClass] = useState(1);
@@ -12,6 +13,7 @@ function App() {
       <Header setSelectedClass={setSelectedClass} />
       <Body selectedClass={selectedClass} />
       <Footer />
+      <Analytics />
     </div>
   );
 }
