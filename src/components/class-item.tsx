d8293f92999b5ef4cc70e3deb126903319239977
bBraian <braianvoficial@gmail.com>
2024-05-23 13:18:19 -0300

    feat: updating dialog styles

--- src/components/class-item.tsx
+++ src/components/class-item.tsx
@@ -11,9 +11,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-// import { Input } from "@/components/ui/input"
-// import { Label } from "@/components/ui/label"
-import { Pencil, Plus } from 'lucide-react';
+
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { CircleCheckBig, CircleX, Pencil, Plus, Save, X } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
 
 
 const whatsIconProps = {
@@ -64,24 +73,80 @@ export function ClassItem({props} :ClassItemProps) {
           <img src={props.image} className="w-6 h-6 rounded-sm mr-2.5 mt-[6px]" alt="" />
           <DialogTitle>{props.title}</DialogTitle>
         </DialogHeader>
-        <div className="flex flex-col p-3 w-full border-2 rounded-md overflow-hidden items-end gap-2">
           <div className="flex w-full items-center gap-2">
-            {/* <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-            /> */}
-            <Button variant="outline" className='w-full'>
-              grupo desenvolvimento web
-            </Button>
-            <Button variant="outline" size="icon">
-              <Pencil className="h-4 w-4" />
-            </Button>
+            
+            <div className='flex flex-col w-full gap-2'>
+              <div className='flex w-full gap-2'>
+                <ContextMenu>
+                  <ContextMenuTrigger className='w-full'>
+                    <Button variant="outline" className='w-full'>
+                      <div className='w-3 h-3 rounded-full bg-green-500 mr-2'></div>
+                      Grupo Desenvolvimento Web 1
+                    </Button>
+                  </ContextMenuTrigger>
+                  <ContextMenuContent>
+                    <ContextMenuItem className='gap-2 text-green-500'><CircleCheckBig className='w-5 h-5' /> Funcionando</ContextMenuItem>
+                    <ContextMenuItem className='gap-2 text-red-500'><CircleX /> Não funcionando</ContextMenuItem>
+                  </ContextMenuContent>
+                </ContextMenu>
+                <Button variant="outline" size="icon">
+                  <Pencil className="h-4 w-4" />
+                </Button>
+              </div>
+              <div className='flex w-full gap-2'>
+                <ContextMenu>
+                  <ContextMenuTrigger className='w-full'>
+                    <Button variant="outline" className='w-full'>
+                      <div className='w-3 h-3 rounded-full bg-green-500 mr-2'></div>
+                      Grupo Desenvolvimento Web 1
+                    </Button>
+                  </ContextMenuTrigger>
+                  <ContextMenuContent>
+                    <ContextMenuItem className='gap-2 text-green-500'><CircleCheckBig className='w-5 h-5' /> Funcionando</ContextMenuItem>
+                    <ContextMenuItem className='gap-2 text-red-500'><CircleX /> Não funcionando</ContextMenuItem>
+                  </ContextMenuContent>
+                </ContextMenu>
+                <Button variant="outline" size="icon">
+                  <Pencil className="h-4 w-4" />
+                </Button>
+              </div>
+              <div className='flex w-full gap-2'>
+                <div className="flex flex-col p-3 w-full border-2 rounded-md overflow-hidden items-end gap-2">
+                  <div className='flex w-full items-center gap-2'>
+                    <Label htmlFor="name" className="text-right">Nome</Label>
+                    <Input
+                      id="name"
+                      defaultValue="Grupo 1"
+                    />
+                    <Button variant="outline" size="icon">
+                      <Save className="h-4 w-4" />
+                    </Button>
+                    <Button variant="outline" size="icon">
+                      <X className="h-4 w-4" />
+                    </Button>
+                  </div>
+                  <div className='flex w-full items-center gap-2'>
+                    <Label htmlFor="link" className="text-right">Link</Label>
+                    <Input
+                      id="link"
+                      defaultValue="https://chat.whatsapp.com/E1QRZ48cQ1U62ThrllpmF4"
+                    />
+                  </div>
+                  
+                  
+                </div>
+                
+              </div>
+              
+            </div>
+            
+            
+            
           </div>
           <Button size='sm' className='flex w-min' variant='secondary'>
             <Plus className="mr-2 h-4 w-4" /> Link Whats
           </Button>
-        </div>
-        <div className="flex flex-col p-3 w-full border-2 rounded-md overflow-hidden items-end gap-2">
+          <Separator />
           <div className="flex w-full items-center gap-2">
             {/* <Input
               id="name"
@@ -97,7 +162,6 @@ export function ClassItem({props} :ClassItemProps) {
           <Button size='sm' className='flex w-min' variant='secondary'>
             <Plus className="mr-2 h-4 w-4" /> Link Drive
           </Button>
-        </div>
         <DialogFooter>
           <Button type="submit">Enviar para aprovação</Button>
         </DialogFooter>
